c5c8b3c81d44337ba0c3d9b3a56ee531076ccb8c
vic6 <vicmatthews116@gmail.com>
2017-03-28 12:49:11 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# create 10 users
 10.times do
-  User.create(first_name: Faker::Name.first_name,
-              last_name: Faker::Name.last_name,
-              user_name: Faker::Pokemon.name,
-              email: Faker::Internet.email,
-              password: Faker::Internet.password)
+  User.create!(first_name: Faker::Name.first_name,
+               last_name: Faker::Name.last_name,
+               user_name: Faker::Pokemon.name,
+               email: Faker::Internet.email,
+               password: Faker::Internet.password)
 end
 
-50.times do {
-  Item.create(name: Faker::
-              description:
-              location_id:
-              user_id:
-              category_id:
-  )
-}
+# create 5 loctions
+5.times do
+  Location.create!(name: Faker::Pokemon.location)
+end
+
+# create 10 categories
+10.times do
+  Category.create(name: Faker::Commerce.department(1))
+end
+
+# create 50 items
+50.times do
+  Item.create(name: Faker::Commerce.product_name,
+              description: Faker::ChuckNorris.fact,
+              location_id: rand(1..5),
+              # grab random user id
+              user_id: rand(1..10),
+              category_id: rand(1..10))
+end
